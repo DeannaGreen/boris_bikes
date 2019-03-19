ac554f4f1a17213cba7599178e3db962bc67ffa0
Deanna Green <deanna@16vmini.co.uk>
2019-03-19 13:27:28 +0000

    Added some more tests, it will now only release a bike if one has been docked otherwise it will print on error message

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,15 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+  @bike = Bike.new
+
   def release_bike
-    bike = Bike.new
+    fail 'No bikes avalible' unless @bike
+    @bike
+  end
+
+  def dock(bike)
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,31 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike)}
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error if there are no bikes avalible' do
+      expect{subject.release_bike}.to raise_error('No bikes avalible')
+    end
+  end
 
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(bike).to be_working
+  end
+
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
